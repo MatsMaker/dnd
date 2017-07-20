d2ae3e32469e8ac302d5ec89d1824dc158f77d71
Sergey Matsitsky <matsitskyss@gmail.com>
2017-07-20 18:16:53 +0300

    base functional

--- src/app/trunk/trunk.component.ts
+++ src/app/trunk/trunk.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 const ITEMS = [{
-  id: 0,
+  id: 1,
   name: 'bike',
   inTrunk: false,
   size: {
@@ -9,7 +9,7 @@ const ITEMS = [{
     height: 2
   },
 }, {
-  id: 1,
+  id: 2,
   name: 'cap',
   inTrunk: false,
   size: {
@@ -17,7 +17,7 @@ const ITEMS = [{
     height: 1
   },
 }, {
-  id: 2,
+  id: 3,
   name: 'camp',
   inTrunk: false,
   size: {
@@ -25,12 +25,28 @@ const ITEMS = [{
     height: 2
   }
 }, {
-  id: 3,
+  id: 4,
   name: 'lamp',
   inTrunk: false,
   size: {
     width: 1,
     height: 3
+  }
+}, {
+  id: 5,
+  name: 'cup',
+  inTrunk: false,
+  size: {
+    width: 1,
+    height: 2
+  }
+}, {
+  id: 6,
+  name: 'sugar',
+  inTrunk: false,
+  size: {
+    width: 1,
+    height: 2
   }
 }]
 
@@ -50,26 +66,10 @@ export class TrunkComponent {
     this.space = this.createSpace(SPACE_SIZE);
   }
 
-  public getItemClass(item) {
+  public getItemClass(itemId) {
+    const item = this.items.find(i => i.id === itemId);
     const stateClassName = (item.inTrunk) ? 'in-trunk' : '';
     return `item width-${item.size.width} height-${item.size.height} ${stateClassName}`;
-  }
-
-  private createSpace(spaceSize) {
-    const space = [];
-    for (let iX = 0; iX < spaceSize[0]; iX++) {
-      for (let iY = 0; iY < spaceSize[1]; iY++) {
-        if (space[iY] === undefined) {
-          space[iY] = [];
-        }
-        space[iY].push({
-          content: null,
-          x: iX,
-          y: iY
-        });
-      }
-    }
-    return space;
   }
 
   public dragStart(event) {
@@ -82,22 +82,68 @@ export class TrunkComponent {
     }
   }
 
-  private droppedItemToPoint(item, pointOfSpace): void {
-    const activeItem = this.items.find(i => i.id === item.id);
-    const lastY = pointOfSpace.y + activeItem.size.height - 1;
-    const lastX = pointOfSpace.x + activeItem.size.width - 1;
-    if (this.space
-      && this.space[lastY]
-      && this.space[lastY][lastX]
-      && this.space[lastY][lastX].content === null) {
+  private createSpace(spaceSize) {
+    const space = [];
+    for (let iX = 0; iX < spaceSize[0]; iX++) {
+      for (let iY = 0; iY < spaceSize[1]; iY++) {
+        if (space[iY] === undefined) {
+          space[iY] = [];
+        }
+        space[iY].push({
+          content: null,
+          headPoint: false,
+          x: iX,
+          y: iY
+        });
+      }
+    }
+    return space;
+  }
 
-      for (let iY = pointOfSpace.y; iY <= lastY; iY++) {
-        for (let iX = pointOfSpace.x; iX <= lastX; iX++) {
-          activeItem.inTrunk = true;
-          this.space[iY][iX].content = activeItem.id;
+  private takeUpPlaces(indexActiveItem, startPoint, toPoint) {
+    const activeItem = this.items[indexActiveItem];
+    for (let iY = startPoint.y; iY <= toPoint.y; iY++) {
+      for (let iX = startPoint.x; iX <= toPoint.x; iX++) {
+        activeItem.inTrunk = true;
+        this.space[iY][iX].content = activeItem.id;
+        if (startPoint.x === iX && startPoint.y === iY) {
+          this.space[iY][iX].headPoint = true;
         }
       }
+    }
+  }
 
+  private clearUpPlace(itemId) {
+    this.space.map(row => {
+      row.map(point => {
+        if (point.content === itemId) {
+          point.content = null;
+          point.headPoint = false;
+        }
+      });
+    });
+  }
+
+  private enoughSpace(fromPoint, toPoint): boolean {
+    return this.space[fromPoint.y][fromPoint.x].content === null
+      && this.space
+      && this.space[toPoint.y]
+      && this.space[toPoint.y][toPoint.x]
+      && this.space[toPoint.y][toPoint.x].content === null;
+  }
+
+  private droppedItemToPoint(itemId, pointOfSpace): void {
+    const indexActiveItem = this.items.findIndex(i => i.id === itemId);
+    const activeItem = this.items[indexActiveItem];
+    const toPoint = {
+      x: pointOfSpace.x + activeItem.size.width - 1,
+      y: pointOfSpace.y + activeItem.size.height - 1
+    };
+    if (this.enoughSpace(pointOfSpace, toPoint)) {
+      if (activeItem.inTrunk) {
+        this.clearUpPlace(activeItem.id);
+      }
+      this.takeUpPlaces(indexActiveItem, pointOfSpace, toPoint);
     }
   }
 }
